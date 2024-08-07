f32632fafd7e380d11a524b3c0bcc5b0da8cba15
Yuma Suzuki <yuuma_23s1103129@nnn.ed.jp>
2024-08-07 14:52:01 +0900

    変更

--- common/loadHeader.js
+++ common/loadHeader.js
@@ -1,4 +1,3 @@
-// main/loadHeader.js
 document.addEventListener("DOMContentLoaded", function() {
     fetch('../main/header/index.html')
         .then(response => response.text())
@@ -6,15 +5,13 @@ document.addEventListener("DOMContentLoaded", function() {
             const placeholder = document.getElementById('header-placeholder');
             placeholder.innerHTML = data;
 
-            // CSSを正しいパスで読み込む
             const cssLink = document.createElement('link');
             cssLink.rel = 'stylesheet';
             cssLink.href = '../main/header/index.css';
             document.head.appendChild(cssLink);
 
-            // JSを正しいパスで読み込む
             const script = document.createElement('script');
-            script.src = '../main/heder/index.js';
+            script.src = '../main/header/index.js';
             document.body.appendChild(script);
         });
 });
---
+++ main/header/index.js
@@ -0,0 +1,24 @@
+const mainTitle = document.getElementById('main-title');
+let isScrolling;
+
+function handleScroll() {
+    clearTimeout(isScrolling);
+
+    isScrolling = setTimeout(() => {
+        if (window.scrollY === 0) {
+            mainTitle.style.opacity = '0';
+            setTimeout(() => {
+                mainTitle.style.display = 'none';
+            }, 1000);
+        } else {
+            mainTitle.style.display = 'block';
+            setTimeout(() => {
+                mainTitle.style.opacity = '1';
+            }, 10);
+        }
+    }, 50);
+}
+
+handleScroll();
+　
+window.addEventListener('scroll', handleScroll);
